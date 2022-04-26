77dc13815d175900468a2f04c724915dd9e1da70
Juan Arias <juanmanuel0560@gmail.com>
2022-04-26 18:30:22 -0500

    Frontend and backend is added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI,Request,Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -7,24 +7,34 @@ import httpx
 app=FastAPI()
 
 
-@app.get("/" , response_class=HTMLResponse)
-async def home(request:Request):
+@app.get("/home/" , response_class=HTMLResponse)
+async def home(request:Request):    
+
     return templates.TemplateResponse("index.html",{
-        "request":request,
-        "message":"Hi Choose your favorite pokemon"
-    })
+    "request":request,
+    "message":"Lista de pokemones",
+    "output1":"Escoje tu pokemon:"
+})
 
+@app.post("/home/",response_class=HTMLResponse)
+async def index(request:Request):
+    pokemon=await request.form()
+    for name in pokemon:
+        names=name
+    return templates.TemplateResponse("index.html",{
+    "request":request,
+    "names":names,
+    "message":f"Habilidades de {names}",
+    "output" : await ability(names)})
 
-@app.get("/pokemon/ability")
-async def read_root():
+@app.get("/ability")
+async def ability(pokemon):
     name_ability=[]
     url_ability=[]
     data_url=[]
-    descrip_data=[]
-    
-    poke_name="ditto"
-    url=f"https://pokeapi.co/api/v2/pokemon/{poke_name}"
-    
+    descrip_data=[]    
+
+    url=f"https://pokeapi.co/api/v2/pokemon/{pokemon}"    
     
     async with httpx.AsyncClient() as client:
         resp= await client.get(url)
@@ -43,19 +53,16 @@ async def read_root():
             descrip=descrip["flavor_text_entries"]
             descrip_data.append(descrip)
 
-    length_descrip_data=len(descrip_data)
-    
-            
+    length_descrip_data=len(descrip_data)            
     
     for description in range(0,length_descrip_data):
         length_internal=len(descrip_data[description])
         for count in range(0,length_internal):
             if descrip_data[description][count]["language"]["name"]=="en" and descrip_data[description][count]["version_group"]["name"]=="sword-shield":
-                data_url.append(descrip_data[description][count]["flavor_text"]) 
-                       
-        
+                data_url.append(descrip_data[description][count]["flavor_text"])
     
-    return data_url
+    returned={name_ability[0]:data_url[0],name_ability[1]:data_url[1]}
+    return returned
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
